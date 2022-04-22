ac5a4e1772382b2744ba9a11a1260e01d9d5f496
Hannah Bush <heb.rutgers@gmail.com>
2022-04-21 21:21:41 -0400

    initial commit, basic files set up

---
+++ server.js
@@ -0,0 +1,19 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//create a static webpage
+app.use(express.static('public'));
+//parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+//parse incoming JSON data
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
