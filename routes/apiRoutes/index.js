07d67685a9b822eeefaebe86f27a46574045cfac
Hannah Bush <heb.rutgers@gmail.com>
2022-05-06 12:12:08 -0400

    note routes switched to index, updated find and add new note

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,36 @@
 const router = require('express').Router();
-const noteRoutes = require('./noteRoutes');
+const fs = require("fs")
+//const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
+let notes = require('../../db/db.json');
 
-router.use(noteRoutes);
+//find all notes
+router.get('/notes', (req, res) => {
+  notes  = JSON.parse(fs.readFileSync("./db/db.json"));
+  res.json(notes);
+});
+
+//find note by id
+router.get('/notes/:id', (req, res) => {
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+//add a new note
+router.post('/notes', (req, res) => {
+  let newNote = {
+    id: Math.floor(Math.random()* 1000),
+    title : req.body.title,
+    text:req.body.text
+  }
+  notes.push(newNote)
+  fs.writeFileSync("./db/db.json",JSON.stringify(notes),function(err){
+    if(err) throw err;
+  })
+  res.json(notes);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,9 +11,11 @@ app.use(express.urlencoded({extended: true}));
 //parse incoming JSON data
 app.use(express.json());
 
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
